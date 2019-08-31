3f7bc4f2bd99bd0fdbc6febca953a53e6f826d47
Vivek Gawande <vivek040997@gmail.com>
2019-08-31 16:29:21 +0530

    Add custom route

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Harry Potter" };
+
+            return View(movie);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex=1, string sortBy="Name")
+        {
+            return Content(String.Format("pageIndex={0} and sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + " / " + month);
+        }
+    }
+}
